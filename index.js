3a2c75a86c87db603e385109e22cce8d6b495068
artur-makeev <artur.makeev.d@gmail.com>
2021-03-07 15:59:52 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1===randomNumber2) {
+  document.getElementsByTagName("h1")[0].innerText = "Draw!";
+}
+else if (randomNumber1 > randomNumber2) {
+  document.getElementsByTagName("h1")[0].innerText = "🚩 Player 1 wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+  document.getElementsByTagName("h1")[0].innerText = "Player 2 wins! 🚩";
+}
